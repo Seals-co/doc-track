4d068b1ad1687f131089f19329f2dc0c59f4e371
Ratinax <72433771+Ratinax@users.noreply.github.com>
2025-07-02 11:39:10 +0200

    Fix setup requirements and add links (#3)
    Authored-by: titouan bernot <titouan.bernot@seals.co>

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import find_packages, setup
 
-
-def parse_requirements(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-
-    return [line.strip() for line in lines if line.strip() and not line.startswith("#")]
-
-
 setup(
     name="doc-track",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
-    install_requires=parse_requirements("requirements.txt"),
+    install_requires=[
+        "iniconfig==2.1.0",
+        "packaging==25.0",
+        "pluggy==1.6.0",
+        "Pygments==2.19.1",
+        "pytest==8.4.1",
+        "setuptools==80.9.0",
+        "tree-sitter==0.24.0",
+        "tree-sitter-python==0.23.6",
+        "pyyaml==6.0.2",
+        "ruff==0.12.1",
+    ],
     entry_points={
         "console_scripts": [
             "doc-track = doctrack.cli:main",
@@ -21,4 +24,9 @@ setup(
     python_requires=">=3.7",
     author="Ratinax",
     description="Command that helps keeping track of piece of code marked as 'documented'",
+    url="https://github.com/Seals-co/doc-track/",
+    project_urls={
+        "Source": "https://github.com/Seals-co/doc-track/",
+        "Tracker": "https://github.com/Seals-co/doc-track/issues/",
+    },
 )
