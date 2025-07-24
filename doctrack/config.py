8f196e22b315129a0302c0ac7b6ee0dd5d540d5f
titouan bernot <titouan.bernot@seals.co>
2025-07-24 10:38:46 +0200

    Remove unnused parameter

--- doctrack/config.py
+++ doctrack/config.py
@@ -19,7 +19,7 @@ def load_config(config_path=".doctrack.yaml"):
 
 def update_args(args, config: dict):
     for key, value in config.items():
-        if key not in ["add_git_message"] and getattr(args, key) is None:
+        if getattr(args, key) is None:
             setattr(args, key, value)
 
     return args
